8d307c18cc0f107a76c226eaa194da13d2bb165d
Clara <clarapachecodev@gmail.com>
2023-03-03 16:56:11 -0300

    Import Container from styles.js and apply this style in Details component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,18 +1,22 @@
-import { Fragment } from "react"
-import './styles.css'
+// import { Fragment } from "react"
+
+import { Container } from './styles.js'
+
+// Podemos criar um component para container, outro para button
+// e por aí em diante -  dessa forma conseguimos deixar tudo
+// separado;
 
 export function Details(){
 
-  
-
   return (
   
-  <Fragment>
+  <Container>
     <h1>Hello World </h1>
     <span>Clara Pacheco</span>
-  </Fragment>
+  </Container>
 
   )
     
   
 }
+

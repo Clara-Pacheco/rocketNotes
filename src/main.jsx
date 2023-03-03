b67cfeaa3732e7df0b478932dc794cc54dca9777
Clara <clarapachecodev@gmail.com>
2023-03-03 16:05:01 -0300

    Create folder for the component Details, containing the index.jsx and styles.css for this specific component + import css file inside index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details'
 
+// por padrão, quando não dizemos qual arquivo queremos carregar,
+// o index será carregado automaticamente;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

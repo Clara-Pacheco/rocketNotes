2993a2587b6bff610c1ac27728869ff50bc66fd2
Clara <clarapachecodev@gmail.com>
2023-03-04 09:05:28 -0300

    Create global styles using createClobalStyle from styled-components, define css rules, import it to main.jsx and apply this component as sibling of Detais component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components' // irá prover um tema
 import theme from './styles/theme'
 import { Details } from './pages/Details'
+import GlobalStyles from './styles/global'
+
 
 // por padrão, quando não dizemos qual arquivo queremos carregar,
 // o index será carregado automaticamente;
@@ -11,7 +13,8 @@ import { Details } from './pages/Details'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Details />
+        <Details />
+      <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>,
 )
